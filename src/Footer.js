4a804ccf1c31f65b575a3822cc23e1c30e6749c7
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-10 23:05:39 +0530

    feat: create footer component

---
+++ src/Footer.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+return (
+    <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+            <Link color="inherit" href="https://codingtricks.io/">
+            CodingtrickS.io
+        </Link>{' '}
+        {new Date().getFullYear()}
+            {'.'}
+    </Typography>
+);
+}
+
+const useStyles = makeStyles((theme) => ({
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
+
+export default function Footer() {
+
+const classes = useStyles();
+
+return (
+<React.Fragment>
+    <CssBaseline />
+
+    <footer className={classes.footer}>
+        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            Technology For Innovators
+        </Typography>
+        <Copyright />
+    </footer>
+
+</React.Fragment>
+);
+}
