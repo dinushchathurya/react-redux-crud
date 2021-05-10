932e06d81e7d3e11072d8cc8e1ce252503d314c9
dinushchathurya <dinushchathurya21@gmail.com>
2021-05-10 23:49:45 +0530

    feat: peerinf footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import React from 'react';
 import './App.css';
 import PostForm from './PostForm';
 import AllPost from './AllPost';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <PostForm />
       <AllPost />
+      <Footer />
     </div>
   );
 }
